78cdb1e0dfa8e0306bad7e1dcc4c05fc3e7ee8b1
Pom Pom <57824016+victoryy2003@users.noreply.github.com>
2022-07-16 11:29:51 +0800

    Storing system - Allow to insert items - Using json database to store items

---
+++ LibSys/functions.py
@@ -0,0 +1,49 @@
+# Title : Library System
+# Programmer : Xian Yee
+# Version : 0.0.0
+
+import json
+
+
+class Store:
+    
+    def __init__(self):
+        pass
+    
+    def Insert(ID, BookTitle, Author, Subject):
+        
+        Database = SysDB.Retrieve()
+        
+        if ID not in Database.keys():
+            Database[ID] = {
+                "BookTitle": BookTitle,
+                "Author": Author,
+                "Subject": Subject
+                            }
+            
+        SysDB.Dump(Database)
+        
+        
+class SysDB:
+    
+    def __init__(self):
+        pass
+    
+    def Retrieve():
+        
+        try:
+            with open("Database.json", "r", encoding = "UTF-8") as f :
+                return json.load(f)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            open("Database.json", "w+", encoding = "UTF-8")
+            return {}
+        
+    def Dump(Database):
+        
+        try:
+            with open("Database.json", "w", encoding = "UTF-8") as f:
+                json.dump(Database, f, indent = 4, ensure_ascii = False, sort_keys = False)
+        except FileNotFoundError:
+            with open("Database.json", "w+", encoding = "UTF-8") as f:
+                json.dump(Database, f, indent = 4, ensure_ascii = False, sort_keys = False)
+        
